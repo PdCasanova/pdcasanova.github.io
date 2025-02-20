2260c13c493166755859696c19f25d57c6a9f4d3
DBACarvalho <daniel.carvalho@fortrevo.com>
2025-02-20 09:08:39 +0000

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// the continue button starts with the display none and after pressing the button verifycode we change the display to block
-const verifyCodeButton = document.getElementById('emailVerificationControl_but_verify_code');
-const continueButton = document.getElementById('continue');
-
-verifyCodeButton.addEventListener('click', function() {
-    continueButton.style.display = 'block'; 
-});
