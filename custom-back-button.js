e29b8189a5d61d93de3351f6c2f44f9468234f7f
DBACarvalho <daniel.carvalho@fortrevo.com>
2025-05-08 10:34:06 +0100

    Update custom-back-button.js

--- custom-back-button.js
+++ custom-back-button.js
@@ -1,47 +1,47 @@
-<script>
-  function goBack() {
-    if (document.referrer && document.referrer !== location.href) {
-      window.history.back();
-    } else {
-      window.location.href = "_CLOSE_THIS_THING";
-    }
-  }
+function insertBackButton() {
+  const apiDiv = document.getElementById('api');
 
-  function insertBackButton() {
-    const apiDiv = document.getElementById('api');
+  if (apiDiv && !document.getElementById('back')) {
+    const backBtn = document.createElement('button');
+    backBtn.id = 'back';
+    backBtn.type = 'button';
+    backBtn.onclick = goBack;
 
-    if (apiDiv && !document.getElementById('back')) {
-      const backBtn = document.createElement('button');
-      backBtn.id = 'back';
-      backBtn.type = 'button';
-      backBtn.onclick = goBack;
+    // Insert your SVG icon
+    backBtn.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg"
+           xmlns:xlink="http://www.w3.org/1999/xlink"
+           fill="#000000"
+           height="24px"
+           width="24px"
+           viewBox="0 0 59.414 59.414"
+           xml:space="preserve">
+        <polygon points="45.268,1.414 43.854,0 14.146,29.707 43.854,59.414 45.268,58 16.975,29.707"/>
+      </svg>
+    `;
 
-      // Insert your custom SVG icon
-      backBtn.innerHTML = '←';
+    // Style the button
+    Object.assign(backBtn.style, {
+      background: 'none',
+      border: 'none',
+      cursor: 'pointer',
+      padding: '10px',
+      marginBottom: '10px',
+      alignSelf: 'flex-start',
+      display: 'flex',
+      alignItems: 'center',
+    });
 
-      // Style it like a minimal back arrow button
-      Object.assign(backBtn.style, {
-        background: 'none',
-        border: 'none',
-        cursor: 'pointer',
-        padding: '10px',
-        marginBottom: '10px',
-        alignSelf: 'flex-start',
-        display: 'flex',
-        alignItems: 'center',
-      });
-
-      const svg = backBtn.querySelector('svg');
+    // Optionally style the SVG
+    const svg = backBtn.querySelector('svg');
+    if (svg) {
       svg.style.height = '24px';
       svg.style.width = '24px';
-
-      // Insert it as the first child of #api
-      apiDiv.insertBefore(backBtn, apiDiv.firstChild);
-      console.log('version 0.1');
-
-      clearInterval(waitForBackBtnInsert);
     }
-  }
 
-  const waitForBackBtnInsert = setInterval(insertBackButton, 300);
-</script>
+    apiDiv.insertBefore(backBtn, apiDiv.firstChild);
+    console.log('version 0.1');
+
+    clearInterval(waitForBackBtnInsert);
+  }
+}
