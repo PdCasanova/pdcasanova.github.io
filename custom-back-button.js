a9a78a32699fed0072077857b0c922f8f18bd64a
DBACarvalho <daniel.carvalho@fortrevo.com>
2025-05-08 13:52:14 +0100

    Update custom-back-button.js

--- custom-back-button.js
+++ custom-back-button.js
@@ -1,40 +1,44 @@
 function goBack() {
-		  if (document.referrer && document.referrer !== location.href) {
-		    window.history.back();
-		  } else {
-		    window.location.href = "_CLOSE_THIS_THING";
-		  }
-		}
-		
-		function insertBackIcon() {
-		  const apiContainer = document.getElementById('api');
-		
-		  // Only proceed if the container exists and no back icon yet
-		  if (apiContainer && !document.getElementById('backIcon')) {
-		    const backIcon = document.createElement('div');
-		    backIcon.id = 'backIcon';
-		    backIcon.onclick = goBack;
-		
-		    // SVG markup for the back arrow
-		    const svg = `
-		      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#333" height="20px" width="20px" version="1.1" id="Capa_1" viewBox="0 0 59.414 59.414" xml:space="preserve">
-		        <polygon points="45.268,1.414 43.854,0 14.146,29.707 43.854,59.414 45.268,58 16.975,29.707"/>
-		      </svg>
-		    `;
+  if (document.referrer && document.referrer !== location.href) {
+    window.history.back();
+  } else {
+    window.location.href = "_CLOSE_THIS_THING"; //To fire the outsystems event where we close the webview
+  }
+}
 
-		    backIcon.innerHTML = svg; // Insert the SVG as the content
-		    backIcon.style.width = "100%";
-		    backIcon.style.textAlign = "start";
-		
-		
-		    // Insert at the beginning of #api container
-		    apiContainer.insertBefore(backIcon, apiContainer.firstChild);
-		
-		    console.log('Version 0.1');
-		
-		    clearInterval(waitForApiContainer);
-		  }
-		}
-		
-		// Check every 300ms until #api container is available
-		const waitForApiContainer = setInterval(insertBackIcon, 300);
+function insertBackIcon() {
+  const apiContainer = document.getElementById('api');
+
+  if (apiContainer && !document.getElementById('backIcon')) {
+    const backIcon = document.createElement('div');
+    backIcon.id = 'backIcon';
+    backIcon.onclick = goBack;
+
+    const svg = `
+      <svg xmlns="http://www.w3.org/2000/svg" fill="#333" height="20px" width="20px" viewBox="0 0 59.414 59.414">
+        <polygon points="45.268,1.414 43.854,0 14.146,29.707 43.854,59.414 45.268,58 16.975,29.707"/>
+      </svg>
+    `;
+
+    backIcon.innerHTML = svg;
+    backIcon.style.width = "100%";
+    backIcon.style.textAlign = "start";
+
+    apiContainer.insertBefore(backIcon, apiContainer.firstChild);
+    console.log('Version 0.2 - using MutationObserver');
+  }
+}
+
+// Use MutationObserver to detect when #api is added to the DOM
+const observer = new MutationObserver((mutations, obs) => {
+  if (document.getElementById('api')) {
+    insertBackIcon();
+    obs.disconnect(); // Stop observing once it's done
+  }
+});
+
+// Start observing the document body for added child nodes
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
