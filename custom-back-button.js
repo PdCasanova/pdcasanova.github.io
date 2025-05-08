66ddab6ad4c080dc37c6d7dd63e17d7371425864
DBACarvalho <daniel.carvalho@fortrevo.com>
2025-05-08 10:21:00 +0100

    Update custom-back-button.js

--- custom-back-button.js
+++ custom-back-button.js
@@ -17,11 +17,7 @@
       backBtn.onclick = goBack;
 
       // Insert your custom SVG icon
-      backBtn.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" fill="rgb(209, 0, 52)" height="24" width="24" viewBox="0 0 59.414 59.414">
-          <polygon points="45.268,1.414 43.854,0 14.146,29.707 43.854,59.414 45.268,58 16.975,29.707 "/>
-        </svg>
-      `;
+      backBtn.innerHTML = '←';
 
       // Style it like a minimal back arrow button
       Object.assign(backBtn.style, {
