8842e56b4fbc0d218ceb10c3e2177e8d9b135dda
DBACarvalho <daniel.carvalho@fortrevo.com>
2025-05-08 09:50:19 +0100

    Update custom-back-button.js

--- custom-back-button.js
+++ custom-back-button.js
@@ -9,22 +9,21 @@
 
   function insertBackButton() {
     const apiDiv = document.getElementById('api');
-    const headingDiv = apiDiv?.querySelector('.heading');
 
-    if (apiDiv && headingDiv && !document.getElementById('back')) {
+    if (apiDiv && !document.getElementById('back')) {
       const backBtn = document.createElement('button');
       backBtn.id = 'back';
       backBtn.type = 'button';
       backBtn.onclick = goBack;
 
-      // Set SVG as button content
+      // Insert your custom SVG icon
       backBtn.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" fill="rgb(209, 0, 52)" height="24" width="24" viewBox="0 0 59.414 59.414">
           <polygon points="45.268,1.414 43.854,0 14.146,29.707 43.854,59.414 45.268,58 16.975,29.707 "/>
         </svg>
       `;
 
-      // Style the button (make it minimal and align left)
+      // Style it like a minimal back arrow button
       Object.assign(backBtn.style, {
         background: 'none',
         border: 'none',
@@ -36,12 +35,13 @@
         alignItems: 'center',
       });
 
-      // Optionally, scale SVG nicely
       const svg = backBtn.querySelector('svg');
       svg.style.height = '24px';
       svg.style.width = '24px';
 
-      apiDiv.insertBefore(backBtn, headingDiv);
+      // Insert it as the first child of #api
+      apiDiv.insertBefore(backBtn, apiDiv.firstChild);
+
       clearInterval(waitForBackBtnInsert);
     }
   }
