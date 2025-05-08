51bbf49615fb9ecc0da48f8e257f4ef9679e08af
DBACarvalho <daniel.carvalho@fortrevo.com>
2025-05-08 14:05:02 +0100

    Update custom-back-button.js

--- custom-back-button.js
+++ custom-back-button.js
@@ -1,44 +1,52 @@
-function goBack() {
-  if (document.referrer && document.referrer !== location.href) {
-    window.history.back();
-  } else {
-    window.location.href = "_CLOSE_THIS_THING"; //To fire the outsystems event where we close the webview
+document.addEventListener('DOMContentLoaded', () => {
+  function goBack() {
+    if (document.referrer && document.referrer !== location.href) {
+      window.history.back();
+    } else {
+      window.location.href = "_CLOSE_THIS_THING";
+    }
   }
-}
 
-function insertBackIcon() {
-  const apiContainer = document.getElementById('api');
+  function insertBackIcon() {
+    const apiContainer = document.getElementById('api');
+    if (apiContainer && !document.getElementById('backIcon')) {
+      const backIcon = document.createElement('div');
+      backIcon.id = 'backIcon';
+      backIcon.onclick = goBack;
 
-  if (apiContainer && !document.getElementById('backIcon')) {
-    const backIcon = document.createElement('div');
-    backIcon.id = 'backIcon';
-    backIcon.onclick = goBack;
+      const svg = `
+        <svg xmlns="http://www.w3.org/2000/svg" fill="#333" height="20px" width="20px" viewBox="0 0 59.414 59.414">
+          <polygon points="45.268,1.414 43.854,0 14.146,29.707 43.854,59.414 45.268,58 16.975,29.707"/>
+        </svg>
+      `;
 
-    const svg = `
-      <svg xmlns="http://www.w3.org/2000/svg" fill="#333" height="20px" width="20px" viewBox="0 0 59.414 59.414">
-        <polygon points="45.268,1.414 43.854,0 14.146,29.707 43.854,59.414 45.268,58 16.975,29.707"/>
-      </svg>
-    `;
+      backIcon.innerHTML = svg;
+      backIcon.style.width = "100%";
+      backIcon.style.textAlign = "start";
 
-    backIcon.innerHTML = svg;
-    backIcon.style.width = "100%";
-    backIcon.style.textAlign = "start";
-
-    apiContainer.insertBefore(backIcon, apiContainer.firstChild);
-    console.log('Version 0.2 - using MutationObserver');
+      apiContainer.insertBefore(backIcon, apiContainer.firstChild);
+      console.log('Inserted back icon');
+    }
   }
-}
 
-// Use MutationObserver to detect when #api is added to the DOM
-const observer = new MutationObserver((mutations, obs) => {
-  if (document.getElementById('api')) {
-    insertBackIcon();
-    obs.disconnect(); // Stop observing once it's done
+  function waitForContainerAndObserve() {
+    const container = document.getElementById('container');
+    if (container) {
+      const observer = new MutationObserver(() => {
+        insertBackIcon();
+      });
+
+      observer.observe(container, {
+        childList: true,
+        subtree: true
+      });
+
+      console.log('Observer attached to #container');
+    } else {
+      // Try again if container not yet in DOM
+      setTimeout(waitForContainerAndObserve, 200);
+    }
   }
-});
 
-// Start observing the document body for added child nodes
-observer.observe(document.body, {
-  childList: true,
-  subtree: true
+  waitForContainerAndObserve();
 });
